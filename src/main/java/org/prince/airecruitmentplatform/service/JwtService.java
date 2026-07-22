36f1f5943cc6f2de4ab51abf63b2380ad1126232
prinxxce21 <princedhakad871@gmail.com>
2026-07-22 23:57:15 +0530

    Add JWT service and project setup for token generation

---
+++ src/main/java/org/prince/airecruitmentplatform/service/JwtService.java
@@ -0,0 +1,26 @@
+package org.prince.airecruitmentplatform.service;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "mySuperSecretKeyForJwtAuthentication123456789123456789123456789";
+
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(key)
+                .compact();
+    }
+
+}
